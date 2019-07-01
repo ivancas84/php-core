40d9f31f1b64ce59397fb1120bea223ea0e01470
Iván <ivancas84@gmail.com>
2019-07-01 13:36:39 -0300

    cargar archivos definidos del core

---
+++ api/checkTransaction.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("class/model/Dba.php");
+
+try{
+  $details = Transaction::checkDetails();
+  echo json_encode(["data" => $details]);
+
+} catch (Exception $ex) {
+  error_log($ex->getTraceAsString());
+  http_response_code(500);
+  echo $ex->getMessage();
+}
