3669831d0c3649aaa27da9e67863a37495855a5c
Iván <ivancas84@gmail.com>
2019-07-17 14:59:08 -0300

    agregar mas opciones a la funcion strto y definir nueva funcion in_array_r similar a in_array, pero acepta array multidimensionales aplicando recursion

--- function/strto.php
+++ function/strto.php
@@ -4,6 +4,8 @@
 
 function strto($string, $format){
   switch($format){
+    case "X": case "X Y": case "XX YY": return mb_strtoupper($string);
+    case "x": case "x y": case "xx yy": return mb_strtolower($string);
     case "XxYy": return str_replace(" ", "", mb_convert_case($string, MB_CASE_TITLE));
     case "xxyy": return str_replace(" ", "", mb_strtolower($string));
     case "Xx Yy": return mb_convert_case($string, MB_CASE_TITLE);
