9fd4e5e10e95035025ec77fa38fc72e5922eaa79
ivan <ivancas84@gmail.com>
2019-10-24 10:57:24 -0300

    reasignar el metodo eliminar_tildes a Format

--- class/Format.php
+++ class/Format.php
@@ -46,4 +46,44 @@ class Format {
     }
   }
 
+  static function removeSpecialCharacters($value){
+    $value = str_replace(
+      array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+      array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+      $value 
+    );
+  
+    $value = str_replace(
+      array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+      array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+      $value 
+    ); 
+  
+    $value = str_replace(
+      array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+      array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+      $value 
+    );
+  
+    $value = str_replace(
+      array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+      array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+      $value 
+    );
+  
+    $value = str_replace(
+      array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+      array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+      $value 
+    );
+  
+    $value = str_replace(
+        array('ñ', 'Ñ', 'ç', 'Ç'),
+        array('n', 'N', 'c', 'C'),
+        $value
+    );
+    
+    return $value;
+  }
+
 }
