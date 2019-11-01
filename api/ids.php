8888b905416299fd089217bd207b442baf81f666
ivan <ivancas84@gmail.com>
2019-11-01 09:23:08 -0300

    reacondicionar api ids

--- api/ids.php
+++ api/ids.php
@@ -5,13 +5,8 @@ require_once("class/model/Dba.php");
 require_once("function/stdclass_to_array.php");
 
 try{
-  $params = Filter::postAll();
-  if(empty($params)) throw new Exception("Parametros no definidos");
-
-  //el uso de parametros es dinamico
-  //se puede definir un parametro opcional "display" que posee un string en formato json para facilitar el uso de tipos basicos
-
-  $render = Dba::renderParams($params);
+  $display = Filter::jsonPostRequired();
+  $render = RenderAux::getInstanceDisplay($display);
   $ids = Dba::ids(ENTITY, $render);
   echo json_encode($ids);
 
