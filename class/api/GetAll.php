e222ebed501ddf27aa59c2d05ff60a77dba52a3f
ivan <ivancas84@gmail.com>
2019-11-25 15:22:03 -0300

    implementar api getAll

---
+++ class/api/GetAll.php
@@ -0,0 +1,26 @@
+<?php
+require_once("class/controller/Dba.php");
+require_once("class/model/RenderAux.php");
+require_once("class/tools/Filter.php");
+
+abstract class GetAllApi {
+  /**
+   * Comportamiento general de all
+   */
+
+  protected $entityName;
+
+  public function main() {
+    try{
+      $ids = Filter::jsonPostRequired();
+      if(empty($ids)) throw new Exception("Identificadores no definidos");
+      $rows = Dba::getAll($this->entityName, $ids);
+      echo json_encode(EntitySqlo::getInstanceRequire($this->entityName)->jsonAll($rows));
+    } catch (Exception $ex) {
+      error_log($ex->getTraceAsString());
+      http_response_code(500);
+      echo $ex->getMessage();
+    }
+  }
+
+}
