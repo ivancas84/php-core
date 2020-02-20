19597f6c254d87c0b473edf0720901ab2e65da77
ivan <ivancas84@gmail.com>
2020-02-20 15:22:01 -0300

    agregar control de cantidad

--- function/array_combine_keys.php
+++ function/array_combine_keys.php
@@ -4,8 +4,10 @@ function array_combine_keys(array $values, $key, $key2){
     /**
      * $values: array asociativo
      * $key: Llave del array asociativo que sera utilizada para combinar, debe ser unica
+     * $key2: Llave del array asociativo que sera utilizada para combinar
      */
-    $keys =  array_column($values, $key);
-    $values_ = array_column($values, $key2);    
+    $keys =  array_unique(array_column($values, $key));
+    $values_ = array_column($values, $key2);
+    if(count($keys) != count($values)) throw new Exception("No pueden combinarse las llaves indicadas, la cantidad de elementos difiere");
     return array_combine($keys, $values_);
 }
