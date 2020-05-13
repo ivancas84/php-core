df6352fbdd7330df77888dfc408794d5fc88943e
unknown <ivancas84@gmail.com>
2020-05-13 07:54:06 -0300

    corregir errores de la implementacion de subida de archivos

--- class/api/Upload.php
+++ class/api/Upload.php
@@ -8,10 +8,15 @@ class UploadApi {
    * Api general de upload
    */
 
-  public $type;
+  public $entityName;
   /**
-   * dependiendo del tipo mime pueden definirse diferente procesamiento
+   * entityName hace referencia principalmente al tipo de archivo
+   * Se definen una serie de tipos,
+   * Dependiendo del tipo mime pueden definirse diferente controlador para su procesamiento
    * El tipo por defecto es File (hace referencia a cualquier archivo)
+   * Otros tipos pueden ser por ejemplo Image (para imagenes) requiere que se almacenen diferentes tamanios para presentar
+   * O puede haber procesamiento especifico de una entidad 
+   * En este sentido entityName puede ser por ejemplo Persona, con su procesamiento particular
    */
 
   public function main() {
