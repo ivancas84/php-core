a79f93261c22e265b240a999cb3ed347ad2aca01
ivancas84 <ivancas84@gmail.com>
2020-06-16 16:47:32 -0300

    Desarrollar nuevo metodo para obtener campos unicos multiples con ciertas caracteristicas

--- class/controller/StructTools.php
+++ class/controller/StructTools.php
@@ -1,13 +1,29 @@
 <?php
 
 class StructTools {
-  public static function getEntityRefBySubtypeSelectNoAdmin($entity){
+  public static function getEntityRefBySubtypeSelect($entity){
     $entities = [];  
     foreach($entity->getFieldsBySubtype("select") as $field){
-        if(!$field->isAdmin()) continue;
         $entityName = $field->getEntityRef()->getName('XxYy');
         if(!key_exists($entityName, $entities)) array_push($entities, $field->getEntityRef());
       }
     return $entities;
   }
+
+  public static function getEntityRefBySubtypeSelectUniqueMultiple($entity){
+    $entities = [];  
+    $fieldsSubtypeSelect = $entity->getFieldsBySubtype("select");
+    $fieldsUniqueMultiple = $entity->getFieldsUniqueMultiple();
+    
+    foreach($fieldsUniqueMultiple as $fieldUM){
+      foreach($fieldsSubtypeSelect as $fieldSS) {
+        if($fieldUM->getName() != $fieldSS->getName()) continue;
+        $entityName = $fieldUM->getEntityRef()->getName('XxYy');
+        if(!key_exists($entityName, $entities)) array_push($entities, $fieldUM->getEntityRef());
+      }
+    }
+    
+    return $entities;
+  }
+
 }
