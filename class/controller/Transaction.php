0bbd90c63235b3f82be391d14e926625590a2bae
ivancas84 <ivancas84@gmail.com>
2020-08-03 07:52:03 -0300

    Adaptar clase Transaction para utilizar nueva clase de acceso a la base de datos

--- class/controller/Transaction.php
+++ class/controller/Transaction.php
@@ -1,37 +1,10 @@
 <?
 
-require_once("class/model/db/Dba.php");
-require_once("class/model/db/My.php");
-require_once("class/model/db/Pg.php");
 require_once("class/tools/FileCache.php");
 
 class Transaction {
 
   public static $id = null; //las transacciones se guardan en sesion mientras se estan ejecutando para poderlas administrar tambien desde el cliente
-  public static $dbInstance = NULL; //conexion con una determinada db
-  public static $dbCount = 0;
-
-  public static function dbInstance() { //singleton db
-    /**
-     * Cuando se abren varios recursos de db instance se incrementa un contador, al cerrarse recursos se decrementa. Si el contador llega a 0 se cierra la instancia de la base
-     */
-    if (!self::$dbCount) {
-      (DATA_DBMS == "pg") ?
-        self::$dbInstance = new DbSqlPg(TXN_HOST, TXN_USER, TXN_PASS, TXN_DBNAME, TXN_SCHEMA) :
-        self::$dbInstance = new DbSqlMy(TXN_HOST, TXN_USER, TXN_PASS, TXN_DBNAME, TXN_SCHEMA);
-    }
-    self::$dbCount++;
-    return self::$dbInstance;
-  }
-
-  public static function uniqId(){ //identificador unico
-    return uniqid();
-    //if(isset($_SESSION["uniqid"])) $_SESSION["uniqid"]++;
-    //else $_SESSION["uniqid"] = intval(date("Ymdhis"));
-    //return $_SESSION["uniqid"];
-    //return hexdec(uniqid());
-    //return strtotime("now");
-  }
 
   public static function begin($id = null){
     /**
@@ -112,14 +85,14 @@ AND updated > '{$status}'
 ORDER BY updated ASC
 LIMIT 20;
 ";
-    $db = self::dbInstance();
+    $db = Db::open(TXN_HOST, TXN_USER, TXN_PASS, TXN_DBNAME);
     $result = $db->query($query);
-    $numRows = intval($db->numRows($result));
+    $numRows = intval($db->num_rows($result));
 
     if($numRows > 0){
       if($numRows == 20) return "CLEAR";
 
-      $rows = $db->fetchAll($result);
+      $rows = $db->fetch_all($result);
 
       $de = [];
       foreach($rows as $row) $de = array_merge($de, explode(",",$row["detail"]));
@@ -144,44 +117,31 @@ LIMIT 20;
      */
     if(empty(self::$id)) throw new UnexpectedValueException("Id de transaccion no definido");
 
-    $dbT = self::dbInstance();
+    $dbT = Db::open(TXN_HOST, TXN_USER, TXN_PASS, TXN_DBNAME);
 
-    try {
-      $id = $dbT->escapeString(self::$id);
-      if(empty($_SESSION["transaction"][self::$id]["description"])) throw new Exception("Transaccion vacia");
-      $description = $_SESSION["transaction"][self::$id]["description"];
-      $descriptionEscaped = $dbT->escapeString($description);  //se escapa para almacenarlo en la base de datos
-      $detail = $dbT->escapeString($_SESSION["transaction"][self::$id]["detail"]);
+    $id = $dbT->escape_string(self::$id);
+    if(empty($_SESSION["transaction"][self::$id]["description"])) throw new Exception("Transaccion vacia");
+    $description = $_SESSION["transaction"][self::$id]["description"];
+    $descriptionEscaped = $dbT->escape_string($description);  //se escapa para almacenarlo en la base de datos
+    $detail = $dbT->escape_string($_SESSION["transaction"][self::$id]["detail"]);
 
-      $type = $dbT->escapeString($_SESSION["transaction"][self::$id]["type"]);
-      $fecha = $_SESSION["transaction"][self::$id]["updated"];
+    $type = $dbT->escape_string($_SESSION["transaction"][self::$id]["type"]);
+    $fecha = $_SESSION["transaction"][self::$id]["updated"];
 
-      $queryTransaction = "
-        INSERT INTO transaction (id, updated, description, detail, type)
-        VALUES ('" . $id . "', '" . $fecha . "', '" . $descriptionEscaped . "', '" .$detail . "', '" . $type . "');
-      ";
+    $queryTransaction = "
+      INSERT INTO transaction (id, updated, description, detail, type)
+      VALUES ('" . $id . "', '" . $fecha . "', '" . $descriptionEscaped . "', '" .$detail . "', '" . $type . "');
+    ";
 
-      $dbT->query($queryTransaction);
-      $dbD = Dba::dbInstance();
-      try {
-        $commitDate = date("Y-m-d H:i:s");
-        $dbD->multiQueryTransaction($description);
-        $dbT->query("UPDATE transaction SET type = 'commit', updated = '" . $commitDate . "' WHERE id = '" . $id . "';");
-        unset($_SESSION["transaction"][self::$id]);
-        self::$id = null;
-        FileCache::set("transaction", $commitDate);
-      } finally {
-        Dba::dbClose();
-      }
-    }
-    finally { 
-      self::dbClose();
-    }
+    $dbT->query($queryTransaction);
+    $dbD = Db::open();
+    $commitDate = date("Y-m-d H:i:s");
+    $dbD->multi_query_transaction($description);
+    $dbT->query("UPDATE transaction SET type = 'commit', updated = '" . $commitDate . "' WHERE id = '" . $id . "';");
+    unset($_SESSION["transaction"][self::$id]);
+    self::$id = null;
+    FileCache::set("transaction", $commitDate);
+
   }
 
-  public static function dbClose() { //cerrar conexiones a la base de datos
-    self::$dbCount--;
-    if(!self::$dbCount) self::$dbInstance->close(); //cuando todos los recursos liberan la base de datos se cierra
-    return self::$dbInstance;
-  }
 }
