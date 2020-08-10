6a54e1d3ef9b0984c6c42358bd5ebde9e987bb6a
ivancas84 <ivancas84@gmail.com>
2020-08-10 17:31:36 -0300

    Incorporar nuevo metodo para facilitar la definicion de Entity

--- class/import/Element.php
+++ class/import/Element.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once("class/tools/Logs.php");
+require_once("class/model/Values.php");
 
 abstract class ImportElement {
     /**
@@ -22,6 +23,15 @@ abstract class ImportElement {
     public function logs() { return $this->logs; }
     abstract function setEntities($data);
     
+    public function setEntity($data, $name, $prefix = ""){
+      /**
+       * Comportamiento por defecto para setear una entidad
+       */
+      $this->entities[$name] = EntityValues::getInstanceRequire($name);
+      $this->entities[$name]->_setDefault();
+      $this->entities[$name]->_fromArray($data, $prefix);
+    }
+
     public function logsEntities(){
         $logs = [];
         foreach($this->entities as $entity) $logs = array_merge($logs, $entity->_logs()->getLogs());
