09497353ff01403b21162357b13c77d8cc512005
ivancas84 <ivancas84@gmail.com>
2020-08-12 00:38:31 -0300

    reestructurar funcion

--- function/strto.php
+++ function/strto.php
@@ -6,13 +6,13 @@ function strto($string, $format, $delimiter = " "){
   //metodo avanzado de formato de strings
   if($delimiter != " ") $string = str_replace($delimiter, " ", $string);
   switch($format){
-    case "X": case "X Y": case "XX YY": $s = preg_replace('/\s\s+/', ' ', mb_strtoupper($string)); break;
-    case "x": case "x y": case "xx yy": $s = preg_replace('/\s\s+/', ' ', mb_strtolower($string)); break;
-    case "XxYy": $s = preg_replace('/\s\s+/', '', mb_convert_case($string, MB_CASE_TITLE)); break;
-    case "xxyy": $s = preg_replace('/\s\s+/', '', mb_strtolower($string)); break;
-    case "Xx Yy": $s = preg_replace('/\s\s+/', ' ', mb_convert_case($string, MB_CASE_TITLE)); break;
+    case "X": case "X Y": case "XX YY": $s = mb_strtoupper($string); break;
+    case "x": case "x y": case "xx yy": $s = mb_strtolower($string); break;
+    case "XxYy": $s =  mb_convert_case($string, MB_CASE_TITLE); break;
+    case "xxyy": $s = mb_strtolower($string); break;
+    case "Xx Yy": $s = mb_convert_case($string, MB_CASE_TITLE); break;
     case "xxYy": 
-      $s = preg_replace('/\s\s+/', '', lcfirst(mb_convert_case($string, MB_CASE_TITLE)));
+      $s = lcfirst(mb_convert_case($string, MB_CASE_TITLE));
       $s = mb_strtlower(mb_substr($s,0,1)).mb_substr($s,1);
     break;
   }
