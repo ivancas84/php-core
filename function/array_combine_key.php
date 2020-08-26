14f24a942bf5f8e20482a7619b8971ab7bbe4a95
ivancas84 <ivancas84@gmail.com>
2020-08-26 08:56:58 -0300

    Emprolijar documentacion interna

--- function/array_combine_key.php
+++ function/array_combine_key.php
@@ -2,8 +2,8 @@
 
 function array_combine_key(array $values, $key){
     /**
-     * $values: array asociativo
-     * $key: Llave del array asociativo que sera utilizada para combinar, debe ser unica
+     * Crear nuevo array, usando $key para las llaves y $values para los valores
+     * $key: Debe ser unica
      */
     return array_combine(array_column($values, $key), $values);
 }
