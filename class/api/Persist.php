640094b309414619c56c1a0f29dbb39adbd55e99
ivancas84 <ivancas84@gmail.com>
2020-08-26 08:54:07 -0300

    Trasladar definicion de valores de salida a controlador de Persistencia

--- class/api/Persist.php
+++ class/api/Persist.php
@@ -14,17 +14,9 @@ class PersistApi {
   public function main(){
     try {
       $data = Filter::jsonPostRequired();
-      /*$data = [
-        ["action"=>"persist", "entity"=>"sede", "row"=>["numero"=>"20", "nombre"=>"Prueba"]],
-        ["action"=>"persist", "entity"=>"asignatura", "row"=>["nombre"=>"Matemática"]]
-      ];*/
       $persistController = Persist::getInstanceRequire($this->entityName);
-      $id = $persistController->main($data);
-      echo json_encode(["id" => $id, "detail" => $persistController->getDetail()]);
-      /**
-       * se devuelve un array con el id principal y el detalle de las entidades persistidas
-       * El detalle es un array donde cada elemento es de la forma "entityId"
-       */
+      $result = $persistController->main($data);
+      echo json_encode($result);
     } catch (Exception $ex) {
       error_log($ex->getTraceAsString());
       http_response_code(500);
