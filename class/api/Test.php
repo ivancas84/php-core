70e43337fd5fe08b6b2dcee47943f5da8fced410
ivancas84 <ivancas84@gmail.com>
2020-09-03 00:44:09 -0300

    Nueva api para verificar el acceso

---
+++ class/api/Test.php
@@ -0,0 +1,23 @@
+<?php
+require_once("class/model/Ma.php");
+require_once("class/model/Render.php");
+require_once("class/tools/Filter.php");
+
+
+class TestApi {
+  /**
+   * Controlador base
+   * Todos los controladores que no se adaptan a las estructuras existentes pueden definirse con Base
+   * El usuario puede retornar el valor que desee 
+   * Data esta pensado para ser llamado a traves de una api
+   * En el caso de que no se utilice una api, conviene utilizar directamente ModelTools.
+   **/
+  
+  public $entityName; 
+  public $container;
+
+  public function main(){
+    return $this->entityName;
+  }
+
+}
