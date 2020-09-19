96c800ab0e9a77e28c9cc621e2760fcca2d06941
ivancas84 <ivancas84@gmail.com>
2020-09-19 16:12:49 -0300

    Corregir errores en la definicion de instancias

---
+++ class/api/FurtherError.php
@@ -0,0 +1,18 @@
+<?php
+require_once("class/model/Ma.php");
+require_once("class/model/Render.php");
+require_once("class/tools/Filter.php");
+
+abstract class FurtherErrorApi {
+  /**
+   * Consulta de errores avanzados de una entidad
+   * Devuelve un par key:value con el primer error encontrado o null si no hay errores
+   * Se define de manera abstracta, se invoca de forma consciente por lo tanto requiere redefinicion
+   */
+
+  public $entityName;
+  public $container;
+
+  abstract public function main();
+
+}
