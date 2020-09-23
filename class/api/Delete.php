63afa8f8ee640c0a758cdade62606eb7613bd963
ivancas84 <ivancas84@gmail.com>
2020-09-22 21:15:35 -0300

    Nueva api para eliminar entidades

---
+++ class/api/Delete.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once("class/tools/Filter.php");
+require_once("class/model/Ma.php");
+
+require_once("class/model/Sqlo.php");
+require_once("class/tools/Validation.php");
+
+require_once("function/stdclass_to_array.php");
+
+class DeleteApi {
+  /**
+   * Comportamiento general de eliminacion
+   * UTILIZAR CON PRECAUCION
+   */
+
+  public $entityName;
+  public $container;
+
+  public function concat($id) {
+    return($this->entityName . $id);
+  }  
+
+  public function main(){
+    $ids = Filter::jsonPostRequired();
+    $sql = $this->container->getSqlo($this->entityName)->deleteAll($ids);
+    $this->container->getDb()->multi_query_transaction_log($sql);
+    $detail = array_map(array($this, 'concat'), $ids);    
+
+    return ["ids" => $ids, "detail" => [$this->entityName.$value->id()]];
+  }
+}
+
+
+
