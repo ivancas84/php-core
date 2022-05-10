289121ebfffa615794b247f14a29dbbf848265b6
ivancas84 <ivancas84@gmail.com>
2022-05-10 16:38:34 -0300

    Incorporar funcion ordinal

---
+++ function/ordinal.php
@@ -0,0 +1,10 @@
+<?php
+
+function ordinal(int $number){
+  $ordinales = array('primero','segundo','tercero','cuarto');
+
+  if ($number<=count ($ordinales)){
+      return $ordinales[$number-1];
+  }
+    return $number.'-esimo';
+}
