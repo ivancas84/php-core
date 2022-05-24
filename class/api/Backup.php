960038805d258ce390d19a57bce75de42de8a316
ivancas84 <ivancas84@gmail.com>
2022-05-24 00:43:11 -0300

    Cambiar nombre por variable generica

--- class/api/Backup.php
+++ class/api/Backup.php
@@ -1,17 +1,15 @@
 <?php
 
-class BackupApi {
+require_once("class/controller/Base.php");
+
+class BackupApi extends BaseController{
   /**
    * Controlador de backup de base de datos
    * El directorio debe existir sino tira error
    **/
   
-  public $entityName; 
-  public $container;
-  public $permission;
-
   public function main(){
-    $filename=date("Ymd_his").'_magistrados.sql';
+    $filename=date("Ymd_his") . '_' . $this->entityName . '.sql';
     $exec = "C:" . DIRECTORY_SEPARATOR. "xampp" . DIRECTORY_SEPARATOR. "mysql" . DIRECTORY_SEPARATOR. "bin" . DIRECTORY_SEPARATOR. "mysqldump -u ". DATA_USER . " " . DATA_DBNAME . " > " . $_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . PATH_BACKUP . DIRECTORY_SEPARATOR . $filename;
     $result=system($exec,$output);
     if($output==''){ return ["file"=>PATH_BACKUP. DIRECTORY_SEPARATOR . $filename]; }
