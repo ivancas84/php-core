7f38599f50cadbb20a7f50a36d42ba478e804d90
ivancas84 <ivancas84@gmail.com>
2022-09-22 13:02:20 -0300

    Agregar clase base

---
+++ class/script/Base.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once("class/controller/Base.php");
+
+abstract class BaseScript extends BaseController {
+  /**
+   * Script api base
+   * Elementos en comun a todos los controladores api
+   **/
+
+}
