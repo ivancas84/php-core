23faa6d3e85bd4061e77e0b6c9f27f43d0aaa4a0
ivancas84 <ivancas84@gmail.com>
2022-11-08 14:03:00 -0300

    Documentacion interna.

--- api/LabelGet.php
+++ api/LabelGet.php
@@ -3,10 +3,6 @@ require_once("function/php_input.php");
 
 
 class LabelGetApi {
-  /**
-   * Comportamiento general de all
-   */
-
   public $entityName;
   public $container;
   public $permission = "r";
@@ -16,6 +12,11 @@ class LabelGetApi {
     
     $id = file_get_contents("php://input");
     if(empty($id)) throw new Exception("Identificador no definido");
+
+    /**
+     * Consultar por id y retornar el id puede resultar redundante, pero es necesario para facilitar 
+     * la implementacion de ciertos elementos como por ejemplo el autocomplete
+     */
     return $this->container->query($this->entityName)->fields(["id","label"])->param("id", $id)->one();
   }
 
